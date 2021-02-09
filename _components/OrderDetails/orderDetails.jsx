2fe85214b7499862e41793f0af201800be1fb0b5
Saiful Islam Rasel <raselcse97@gmail.com>
2021-02-08 17:14:42 -0800

    manage orderDetails component

--- _components/OrderDetails/orderDetails.jsx
+++ _components/OrderDetails/orderDetails.jsx
@@ -1,5 +1,38 @@
+import styles from './OrderDetails.module.css'
+
 export default function OrderDetails({order}){
     return (
-        <p>I am order {order.id}</p>
+        <div className={styles.container}>
+            <p>Order ID: {order.id}</p>
+            <p>Custmer Name : {order.customer.name}</p>
+            <p>Customer Mobile: {order.customer.phone}</p>
+            <p>Customer Address: {order.customer.address}</p>
+            <p>Product Details: </p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Title</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        order.products.map(item => {
+                            return (
+                                <tr key={item.id}>
+                                    <td>{item.id}</td>
+                                    <td>{item.title}</td>
+                                    <td>{item.quantity}</td>
+                                    <td>{item.price}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+            <p>Total Price: {order.totalPrice}</p>
+        </div>
     )
 }
